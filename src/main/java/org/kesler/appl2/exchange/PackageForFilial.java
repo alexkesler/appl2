d2847e42a3791aaf167662dc2e70a6e9c87a50e9
Alexey <kesler@mail.ru>
2014-01-29 20:19:12 +0600

    begin to realize XML exchange

---
+++ src/main/java/org/kesler/appl2/exchange/PackageForFilial.java
@@ -0,0 +1,12 @@
+package org.kesler.appl2.exchange;
+
+public class PackageForFilial {
+
+	private Filial filial;
+
+	private List<Service> services;
+
+	private List<ReceptionStatus> statuses;
+
+	// + выходные дни
+}
